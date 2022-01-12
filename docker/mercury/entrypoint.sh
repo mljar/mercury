194f5340262e63bc5b324a3ded33aaa93e90033c
Piotr <pplonski86@gmail.com>
2022-01-12 10:33:27 +0100

    set notebooks path as env variable in docker deploy

--- docker/mercury/entrypoint.sh
+++ docker/mercury/entrypoint.sh
@@ -5,6 +5,10 @@ do
     echo "Waiting for server volume..."
 done
 
+# clear old data dbs
+rm *.sqlite
+rm *.sqlite3
+
 until python manage.py migrate
 do
     echo "Waiting for db to be ready..."
@@ -14,22 +18,24 @@ done
 # path with jupyter
 export PATH="$HOME/.local/bin:$PATH"
 
-# echo "Add requirements for notebooks"
-# REQ=/app/notebooks/requirements.txt
-# if test -f "$REQ"; then
-#     pip install -r $REQ
-# fi
+echo "Add requirements for notebooks"
+REQ=/app/notebooks/requirements.txt
+if test -f "$REQ"; then
+    pip install -r $REQ
+fi
 
-# echo "Add notebooks"
-# for filepath in /app/notebooks/*.ipynb; do
-#     python manage.py add_notebook $filepath
-# done
+echo "Add notebooks"
+for filepath in /app/notebooks/*.ipynb; do
+    python manage.py add $filepath
+done
 
-DEBUG=False python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
-DEBUG=False celery -A server worker --loglevel=error -P gevent --concurrency 4 -E &
+python manage.py createsuperuser --noinput
 
-DEBUG=False gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
+celery -A server worker --loglevel=error -P gevent --concurrency 4 -E &
+
+gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
 
 # for debug
 # python manage.py runserver 0.0.0.0:8000
