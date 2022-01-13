6aacd9fbef52ce09414d01666abe2ba7cde8e421
Piotr <pplonski86@gmail.com>
2022-01-13 11:35:24 +0100

    set version 0.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def list_files(directory):
 
 setup(
     name="mljar-mercury",
-    version="0.0.9",
+    version="0.1.0",
     maintainer="MLJAR Sp. z o.o.",
     maintainer_email="contact@mljar.com",
     description="Share your Python Notebooks with others",
