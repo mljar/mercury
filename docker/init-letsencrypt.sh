81f8b18d48092925cacd55979baa5cd069c06645
Piotr <pplonski86@gmail.com>
2022-03-14 13:25:39 +0100

    add docker-compose for deployment with HTTPS (#64)

--- docker/init-letsencrypt.sh
+++ docker/init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=({{your_domain}} www.{{your_domain}})
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
 email="" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
--- setup-pro.sh
+++ setup-pro.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-
+echo "Setup Mercury Pro"
 if [[ $# -ne 1 ]]; then
     echo "Please specify your domain" >&2
     exit 2
 fi
-
 echo "Set domain to $1 in docker/init-letsencrypt.sh" 
 sed -i "s/{{your_domain}}/$1/g" docker/init-letsencrypt.sh
-
 echo "Set domain to $1 in docker/nginx/pro/default.conf" 
 sed -i "s/{{your_domain}}/$1/g" docker/nginx/pro/default.conf
-
 echo "[Done] Domain set"
 echo "---------------------------------------------------------"
 mv docker/init-letsencrypt.sh init-letsencrypt.sh
-echo "Please run following commands:"
-echo "sudo ./init-letsencrypt.sh"
-echo "sudo docker-compose -f docker-compose-pro.yml up --build -d"
+echo "Build docker-compose:"
+sudo docker-compose -f docker-compose-pro.yml build
+echo "[Done] Docker-compose build"
+echo "---------------------------------------------------------"
+echo "Initialize SSL certificates"
+sudo ./init-letsencrypt.sh
+echo "[Done] SSL certificates issued"
+echo "---------------------------------------------------------"
+echo "Start service"
+sudo docker-compose -f docker-compose-pro.yml up --build -d
+echo "[Done] Mercury is running"
+echo "---------------------------------------------------------"
