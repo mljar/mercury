8f4fabcb96148c1f15e0ff2a90b35c4fc26a60c0
Piotr <pplonski86@gmail.com>
2022-05-25 10:14:31 +0200

    set version 0.8.6

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def list_files(directory):
 
 setup(
     name="mljar-mercury",
-    version="0.8.5",
+    version="0.8.6",
     maintainer="MLJAR Sp. z o.o.",
     maintainer_email="contact@mljar.com",
     description="Convert Python Notebook to web app and share with others",
