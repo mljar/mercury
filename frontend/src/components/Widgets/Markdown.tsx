2e269c8c3a1c87d97857e62f4b53ba6e2afb28d7
Piotr <pplonski86@gmail.com>
2022-06-30 13:34:29 +0200

    add Markdown widget (#87)

---
+++ frontend/src/components/Widgets/Markdown.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+import ReactMarkdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import remarkGfm from "remark-gfm";
+import emoji from "remark-emoji";
+import rehypeRaw from "rehype-raw";
+
+type MarkdownProps = {
+  value: string;
+};
+
+export default function MarkdownWidget({ value }: MarkdownProps) {
+  return (
+    <div className="form-group mb-3">
+      <ReactMarkdown
+        rehypePlugins={[remarkGfm, rehypeHighlight, emoji, rehypeRaw]}
+      >
+        {value}
+      </ReactMarkdown>
+    </div>
+  );
+}
