9defbe8c3109d4cf62f1488e68bc9a134f9c9900
Piotr <pplonski86@gmail.com>
2023-02-15 14:23:34 +0100

    set version 2.0.4

--- mercury/__init__.py
+++ mercury/__init__.py
@@ -1,4 +1,4 @@
 
-__version__ = "2.0.3"
+__version__ = "2.0.4"
 
 from mercury.mercury import *
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def list_files(directory):
 
 setup(
     name="mercury",
-    version="2.0.3",
+    version="2.0.4",
     maintainer="MLJAR Sp. z o.o.",
     maintainer_email="contact@mljar.com",
     description="Turn Jupyter Notebook to Web App and share with non-technical users",
