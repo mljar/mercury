9e9e0db76ba5616556f52edfb56396079eee92c0
Piotr <pplonski86@gmail.com>
2023-03-31 16:10:05 +0200

    add info about user in file rest api (#226)

--- mercury/apps/storage/serializers.py
+++ mercury/apps/storage/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
+from dj_rest_auth.serializers import UserDetailsSerializer
+
 from apps.storage.models import UploadedFile
 
 
+
+    
+
 class UploadedFileSerializer(serializers.ModelSerializer):
+    created_by = UserDetailsSerializer(many=False, read_only=True)
+
     class Meta:
         model = UploadedFile
         read_only_fields = ("id", "created_at", "created_by")
