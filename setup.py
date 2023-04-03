69b8b83e71073f2e5a265b69cdca5ac3ceef1bd4
Piotr <pplonski86@gmail.com>
2023-04-03 10:21:55 +0200

    set version 2.1.8

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def list_files(directory):
 
 setup(
     name="mercury",
-    version="2.1.7",
+    version="2.1.8",
     maintainer="MLJAR Sp. z o.o.",
     maintainer_email="contact@mljar.com",
     description="Turn Jupyter Notebook to Web App and share with non-technical users",
