e5a76ae3ed104a4a72c7728c726c63f58409e2b8
Piotr <pplonski86@gmail.com>
2023-04-05 15:17:06 +0200

    set workers per machine

--- mercury/server/settings.py
+++ mercury/server/settings.py
@@ -291,7 +291,7 @@ LOGGING = {
     },
 }
 
-NBWORKERS_PER_MACHINE = 20
+NBWORKERS_PER_MACHINE = int(os.environ.get("NBWORKERS_PER_MACHINE", 20))
 
 # delta time after which worker is considered as stale and deleted
 WORKER_STALE_TIME = 1  # in minutes
