d5726c84755fae0c49ed5ffa0aeb35ce2fc68fde
Piotr <pplonski86@gmail.com>
2023-10-11 09:58:36 +0200

    set version 2.3.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def list_files(directory):
 
 setup(
     name="mercury",
-    version="2.3.4",
+    version="2.3.5",
     author="MLJAR Sp. z o.o.",
     maintainer="MLJAR Sp. z o.o.",
     maintainer_email="contact@mljar.com",
