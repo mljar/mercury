342cc04d692487ae6fa2683353cb30d8961e8334
Piotr <pplonski86@gmail.com>
2026-08-28 09:18:50 +0200

    chore: bump version to 3.2.6

--- mercury/__init__.py
+++ mercury/__init__.py
@@ -37,7 +37,7 @@ is no `main()` function and no server entry point to write.
 Docs: https://runmercury.com/docs/
 """
 
-__version__ = "3.2.5"
+__version__ = "3.2.6"
 
 from IPython.display import display
 
